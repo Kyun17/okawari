e5eb9a0caa486cd30482ba9753b90041ee9d278a
Kyun17 <tldnrjsdn123@naver.com>
2025-11-05 16:48:36 +0900

    Initial project structure from Spring Initializr

---
+++ src/main/java/dev/okawari/OkawariApplication.java
@@ -0,0 +1,13 @@
+package dev.okawari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkawariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkawariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/okawari/OkawariApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.okawari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkawariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
