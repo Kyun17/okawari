c9aba0a1b04df5c80f047926e0b119eb61a47d31
gunu00 <122669173+Kyun17@users.noreply.github.com>
2025-11-06 14:19:29 +0900

    refacor/파일 구조 정리

--- src/main/java/dev/okawari/entity/Review.java
+++ src/main/java/dev/okawari/entity/Review.java
@@ -1,13 +1,5 @@
-@Entity
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class Review{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    
-    private int id;
-    private String content;
-    private int rating;
+package dev.okawari.entity;
+
+public class Review {
+
 }
